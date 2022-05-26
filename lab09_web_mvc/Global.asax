74c38d372259a5ce2993a8ed059ee78712a70a59
Miguel Angel Inga Ninahuanca <miguel.inga@tecsup.edu.pe>
2022-05-25 23:28:41 -0500

    Add project files.

---
+++ lab09_web_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab09_web_mvc.MvcApplication" Language="C#" %>
---
+++ lab09_web_mvc/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab09_web_mvc.Models
+{
+    public class PersonModel
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
