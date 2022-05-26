baf38636ca9ed4a6dd91421c006f9db2b05a6ba0
Miguel Angel Inga Ninahuanca <miguel.inga@tecsup.edu.pe>
2022-05-25 23:50:59 -0500

    ProductController

---
+++ lab09_web_mvc/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using lab09_web_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab09_web_mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            if (Session["products"] == null)
+                Session["products"] = new List<ProductModel>();
+            return View(Session["products"]);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = ((List<ProductModel>)Session["products"]).Where(x => x.ProductId == id).FirstOrDefault();
+
+            return View(model);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ((List<ProductModel>)Session["products"]).Add(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = ((List<ProductModel>)Session["products"]).Where(x => x.ProductId == id).FirstOrDefault();
+            return View(model);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ lab09_web_mvc/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab09_web_mvc.Models
+{
+    public class ProductModel
+    {
+        public int ProductId{ get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
